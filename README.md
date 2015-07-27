cd1c1dc9b4d2fa729e3454658a0614ba103ca0f3
VarianOnesky <VarianNg@Varians-iMac.local>
2015-07-27 23:48:28 +0800

    Initialisation

---
+++ README.md
@@ -0,0 +1,4 @@
+BabyLog
+=======
+
+A Symfony project created on July 27, 2015, 11:38 pm.
---
+++ src/Baby/LogBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Baby\LogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('LogBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
